e174b119776b108ced68c39a3908f563c937ea6f
nomura <nomura@m.cs.osakafu-u.ac.jp>
2019-12-31 15:26:20 +0900

    init profileItem

---
+++ src/components/ProfileItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="profile-item card text-center w-100"
+    ontouchstart=""
+    v-on:click="turn()"
+  >
+    <div v-if="!is_reversed">
+      <div class="profile-title card-header">
+        {{ title }}
+      </div>
+
+      <div class="profile-body card-body p-2">
+        {{ description }}
+      </div>
+    </div>
+
+    <div v-else>
+      <p
+        style="white-space: pre-line;"
+        v-text="comment"
+        class="card-body p-2"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ProfileItem',
+    props: {
+      title: String,
+      description: String,
+      comment: String
+    },
+    data() {
+      return {
+        is_reversed: false
+      }
+    },
+    methods: {
+      turn() {
+        this.is_reversed = !this.is_reversed
+      }
+    }
+  }
+</script>
+
+<style>
+  .profile-item {
+    filter: drop-shadow(0 5px 10px rgba(0, 0, 0, 0.6));
+    margin-top: 10px;
+  }
+  .profile-item:active {
+    background-color: rgb(102, 102, 102);
+  }
+</style>
--- src/views/page1.vue
+++ src/views/page1.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="page1" id="page1">
     <h1>This is Page1</h1>
+    <profile-item
+      v-bind:title="'Title1'"
+      v-bind:description="'description'"
+      v-bind:comment="'comment1'"
+    />
   </div>
 </template>
+
+<script>
+  import ProfileItem from '../components/ProfileItem.vue'
+  export default {
+    components: {
+      ProfileItem
+    }
+  }
+</script>
 
 <style scoped>
   .page1 {
